b01ca38860c677af96e122e43bc9eee1b2053410
abbas usman <johnmccain2244@gmail.com>
2024-01-30 21:32:33 +0900

    fix: watch project standarts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,15 +4,8 @@ import { Box, Button, Container, Stack, Typography } from '@mui/material';
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <Stack flexDirection={"column"}>
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component={"h1"} gutterBottom>
-            Create React App on TypeScript with REDUX
-          </Typography>
-        </Box>
-        <Button variant="contained">Contained</Button>
-      </Stack>
+    <Container >
+      
    </Container>
   );
 }
